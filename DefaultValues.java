1155bc30a2f05253826e45d0b46e92db65842d7a
swetha-192 <swethakapse19@gmail.com>
2025-02-06 13:56:13 +0530

    Add files via upload

---
+++ DefaultValues.java
@@ -0,0 +1,26 @@
+class DefaultValues {
+// Instance variables with default values
+byte defaulyByte;
+short defaultShort;
+int defaultInt;
+long defaultLong;
+float defaultFloat;
+double defaultDouble;
+char defaultChar;
+boolean defaultBoolean;
+
+public static void main(strings[] args) {
+    DefaultValues obj = new DefaultValues();
+    System.out.println("Default values of primitive data types:");
+    System.out.println("byte: "+ obj.defaultByte);
+    System.out.println("short: "+ obj.defaultShort);
+    System.out.println("int: " + obj.defaultInt);
+    System.out.println("long: "+ obj.defaultLong);
+    System.out.println("float: " + obj.defaultFloat);
+    System.out.println("double: " + obj.defaultDouble);
+    System.out.println("char: [" + obj.defaultChar + "]");
+// Empty character 
+    System.out.println("boolean: " + obj.defaultBoolean);
+  }
+}
+    
